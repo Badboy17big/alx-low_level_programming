e97840206ed1acdd4570117ece64004c74ea686d
abdllrahman <astro71big@gmail.com>
2023-07-17 11:34:43 -0700

    a

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -  Entry point
+ *
+ * descreption: using putchar for the alphabtical
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	char Lch = 'a';
+
+	while (Lch <= 'z')
+	{
+		if (Lch == 'q' || lch == 'e')
+			ch++;
+		putchar(Lch);
+		Lch++;
+	}
+}
