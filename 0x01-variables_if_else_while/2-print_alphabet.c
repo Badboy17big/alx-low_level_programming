ebdbd372e3322fd1081e327e691280210cafc95f
abdllrahman <astro71big@gmail.com>
2023-07-17 10:05:57 -0700

    a

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main -  Entry point
+ *
+ * descreption: alphabtical lower case from a to z
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z') 
+	{
+		putchar(ch);
+       		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
