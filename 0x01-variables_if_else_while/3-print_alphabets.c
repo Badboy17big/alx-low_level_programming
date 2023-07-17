692ebb893f7fd034f099e670d3ead67d89892d9e
abdllrahman <astro71big@gmail.com>
2023-07-17 11:20:15 -0700

    a

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main -  Entry point
+ *
+ * descreption: using putchar for the alphabtical
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	char Uch = 'A', Lch = 'a';
+
+	while (Lch <= 'z')
+	{
+		putchar(Lch);
+		Lch++;
+	}
+	while (Uch <= 'Z')
+	{
+		putchar(Uch);
+		Uch++;
+	}
+	return (0);
+}
