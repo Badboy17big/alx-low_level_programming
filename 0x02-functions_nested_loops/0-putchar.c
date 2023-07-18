82e373c2df65ad176c782c838db6a0840b0a50ea
abdllrahman <astro71big@gmail.com>
2023-07-18 16:57:16 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -  Entry point
+ *
+ * descreption: using sizeof to print the size of various types on the computer
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
