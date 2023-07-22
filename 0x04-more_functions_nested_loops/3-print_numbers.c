66946e3d14381ff2d03acb1b69b5ac70cf912cd5
abdllrahman <astro71big@gmail.com>
2023-07-21 17:15:35 -0700

    a

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,15 @@
 #include "main.h"
 
 /**
- * print_numbers - print 0 - 9
- *      only using _putchar twice
+ * mul - multiplies two integers
  *
- * Return: Always 0 (Success)
-*/
+ * @a: first input
+ * @b: second input
+ *
+ * Return: return results
+**/
 
-void print_numbers(void)
+int mul(int a, int b)
 {
-	int num = 0;
-
-	do {
-	_putchar(num + 48);
-	num++;
-	} while (num >= 0 && num <= 9);
-	_putchar('\n');
+	return (a * b);
 }
