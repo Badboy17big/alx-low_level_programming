8037f650f4339f4a29a3929745933f8b42d6c694
abdllrahman <astro71big@gmail.com>
2023-07-27 13:24:15 -0700

    a

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,33 +1,34 @@
 #include <stdio.h>
 
 /**
- * main -  Entry point
- *
- * descreption: using sizeof to print the size of various types on the computer
- *
- * Return: always 0 (success)
-*/
-
-
+ * main - prints all possible combinations of two two-digit numbers
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
+	int i, j;
 
-	a[2] = 1024;
-	p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-	*(p + 5) = 98;
-  /* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+		}
+	}
+	putchar('\n');
 	return (0);
 }
 
