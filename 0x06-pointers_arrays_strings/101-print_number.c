eadbb7c83369729b3c8e4bf6d0aa39ffe29b010d
abdllrahman <astro71big@gmail.com>
2023-07-27 13:31:27 -0700

    a

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,34 +1,26 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - prints all possible different combinations of three digits
- * Return: Always 0 (Success)
+ * print_number - print numbers chars
+ * @n: integer params
+ * Return: 0
  */
-int main(void)
+
+void print_number(int n)
 {
-	int n, m, l;
+	unsigned int n1;
 
-	for (n = 48; n < 58; n++)
+	n1 = n;
+
+	if (n < 0)
 	{
-		for (m = 49; m < 58; m++)
-		{
-			for (l = 50; l < 58; l++)
-			{
-				if (l > m && m > n)
-				{
-					putchar(n);
-					putchar(m);
-					putchar(l);
-					if (n != 55 || m != 56)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
-		}
+		_putchar('-');
+		n1 = -n;
 	}
-	putchar('\n');
-	return (0);
-}
 
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
+}
